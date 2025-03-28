304a6930e1817b056f8f8e58f030cb1360d5d82e
Uros Petrovic <urospetrovic2070@gmail.com>
2025-03-28 23:29:57 +0100

    Removed the App Component from the Client Entry Point

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
 
 hydrateRoot(
   document.getElementById("root"),
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <BrowserRouter></BrowserRouter>
   </React.StrictMode>
 );
